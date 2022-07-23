abb5c7eb7a111a7c28d5f4e955d2ee2a3c752cf5
malickbax <malickbax@gmail.com>
2022-07-23 13:20:19 -0400

    added jest to package.json

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,23 @@
+// Test the validity of adding a Manager 
+new const Manager = require("../lib/Manager");
+
+// If an office number for Manager is created, it should return the tested office number
+test("Can create an office number.", () => {
+    const testOfficeNumber = 305;
+    const sampleEmployee = new Manager("Malick Ba", 305, "testing@email.com", testOfficeNumber);
+    expect(sampleEmployee.officeNumber).toBe(testOfficeNumber);
+});
+
+// If an office number is entered it should return it
+test("Testing officeNumber will return office number.", () => {
+    const testOfficeNumber = 305;
+    const sampleEmployee = new Manager("Malick Ba", 305, "testing@email.com", testOfficeNumber);
+    expect(sampleEmployee.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+// Application should return "Manager" as the employee role
+test("Testing role.", () => {
+    const returnValue = "Manager";
+    const sampleEmployee = new Manager("Malick Ba", 305, "testing@email.com", 2);
+    expect(sampleEmployee.getRole()).toBe(returnValue);
+});
